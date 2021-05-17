73f39ace56be27cee8b020983c191c57d2889148
Deon Graicas <deongracias1@gmail.com>
2021-05-17 21:51:38 +0530

    Restyled

--- pages/project.php
+++ pages/project.php
@@ -1,96 +1,53 @@
-<<<<<<< HEAD:pages/project.php
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../styles/globals.css">
+    <link rel="stylesheet" href="../styles/navbar.css">
+    <link rel="stylesheet" href="../styles/main.css">
+    <title>Project</title>
 </head>
+
 <body>
-    <?php include 'menubar.php';?>
 
-    <div class="container-1">
-        <h4>Projects</h4>
-    </div>
-    <?php
-        $a = array(10,9,8,7,6,5,4,3,2,1);
-        $prj = 'project name';
-        foreach ($a as $value) {
-          echo '<div class="container">
-            <div class="top_line">'
-               ."<p> $prj </p>".
-                '<button class="material-icons delete-btn"> delete </button>'.
-            '</div>
-            <div class="bottom_line">'
-                ."<p>Start:dd/mm/yy</p>
-                <p>End:dd/mm/yy</p>".
-            '</div>
-            </div>';
-           
-        }
-    ?>
-        <a href="adduser.php" class="material-icons add-btn" 
-         style="bottom: 110px"
-        >person_add</a>
-        <a href="addprj.php" class="material-icons add-btn">add</a>
-    
-    
-    
-</body>
-=======
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
-</head>
-<body>
-    <?php include '../../menubar.php';?>
-
-    <div class="container-1">
-        <h4>Projects</h4>
-    </div>
-    <?php
-        $a = array(10,9,8,7,6,5,4,3,2,1);
-        $prj = 'project name';
-        foreach ($a as $value) 
-        {
-    ?>
-        <div class="container">
-            <div class="top_line">
-               <p> <?php echo $prj ?> </p>
-                <button class="material-icons delete-btn"> delete </button>
+    <?php include '../components/menubar.php'; ?>
+    <div class="container">
+        <div class="card-container">
+            <div class="header">
+                Project
             </div>
-
-            <div class="bottom_line">
-                <p>Start:dd/mm/yy</p>
-                <p>End:dd/mm/yy</p>
-            </div>
+            <?php
+            $a = array(10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+            $issue = 'Project Name';
+            foreach ($a as $value) {
+            ?>
+                <div class="card">
+                    <div class="card-details">
+                        <div class="title" href="issuedetail.php">
+                            <?php echo $issue . $value ?>
+                        </div>
+                        <div class="start-date">Start:dd/mm/yy</div>
+                        <div class="end-date">End:dd/mm/yy</div>
+                    </div>
+                    <button class="card-button">
+                        <i class="fas fa-trash"></i>
+                    </button>
+                    </button>
+                </div>
+            <?php
+            }
+            ?>
         </div>
-    <?php
-           
-        }
-    ?>
-        <a href="adduser.php" class="material-icons add-btn" 
-         style="bottom: 110px"
-        >person_add</a>
-        <a href="addprj.php" class="material-icons add-btn">add</a>
-    
-    
-    
+        <a href="addissue.php" class="add-btn">
+            <i class="fas fa-sm fa-plus"></i></a>
+    </div>
+
+
 </body>
->>>>>>> b1aba4acf057b25d4da68ec4258d9f84bd09353d:components/project/project.php
+
 </html>
