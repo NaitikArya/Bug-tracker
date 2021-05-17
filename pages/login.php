ee9ed812a0e9e92c5db5e23c537daf83f8d82dcf
Deon Graicas <deongracias1@gmail.com>
2021-05-17 20:03:01 +0530

    Added Comments

--- pages/login.php
+++ pages/login.php
@@ -6,7 +6,6 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="../styles/globals.css">
-	<link rel="stylesheet" href="../styles/navbar.css">
 	<link rel="stylesheet" href="../styles/main.css">
 	<title>Login</title>
 </head>
@@ -17,8 +16,14 @@
 			<div class="header">
 				<h1>Login</h1>
 			</div>
+
+			<!-- Username -->
 			<input type="text" placeholder=" Enter Username" class="textfield" name="username" required>
+
+			<!-- Password -->
 			<input type="password" placeholder="Enter Password" class="textfield" name="password" required>
+
+			<!-- Submit Button -->
 			<button type="submit" class="submit-btn">Login</button>
 			<p>New here? Click here to <a href="signup.php">Sign Up</a></p>
 		</form>
