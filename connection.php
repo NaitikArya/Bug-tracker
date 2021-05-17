a14f1272aa9b292c3d9d8ee34a8d5c273c997256
NaitikArya <77172282+NaitikArya@users.noreply.github.com>
2021-05-17 16:16:55 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,30 @@
+<?php
+    class database_connection {
+        private $servername;
+        private $dbname;
+        private $user; 
+        private $pass;
+
+        public function connection()
+        {
+           try {
+                $this->servername = "localhost";
+                $this->dbname = "bug_tracking";
+                $this->user = "root";
+                $this->pass = "";
+
+                $dbcon = new PDO("mysql:host=$this->servername; dbname=$this->dbname",$this->user,$this->pass);
+                $dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $dbcon->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+
+                return $dbcon;
+            } 
+            catch (PDOException $e) {
+               echo "Database Connection failed: ". $e->getMessage();
+            }
+            
+        }
+
+    }
+    
+?>
