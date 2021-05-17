456bbc52aff0b6f0bb5b9e08eb34c92fcb567dec
Deon Graicas <deongracias1@gmail.com>
2021-05-17 22:51:50 +0530

    Removed connection redundancy

--- pages/signin.php
+++ pages/signin.php
@@ -1,89 +1,35 @@
-<<<<<<< HEAD:pages/signin.php
 <?php
-	include 'connection.php';
-	$obj = new database_connection();
-    //$obj->connection();
+include 'connection.php';
+$obj = new database_connection();
+//$obj->connection();
 
-	$user = $_POST['username'];
-	$password = md5($_POST['password']);
-	//echo "<script> alert('$user') </script>";
-		//select statement is used to get the value of username and password from database
-		$select= "SELECT username, passwords FROM user";
-		
-		$result= $obj->connection()->query($select);
-		if ($result -> rowCount() >= 0) 
-		{
-			while ($row = $result -> fetch()) 
-			{
-				//echo "username ".$row["username"]."<br>";
-				if ($row["username"]== $user && $row["passwords"]== $password) 
-				{
-					echo "login successful<br>";
+$user = $_POST['username'];
+$password = md5($_POST['password']);
+//echo "<script> alert('$user') </script>";
+//select statement is used to get the value of username and password from database
+$select = "SELECT username, passwords FROM user";
 
-                    header("Location: project.php");
-					
-				    // send mail code
-					$subject="Registration successful";
-					$message="Welcome to our page. <br> thank u";
-					$from="From: Naitik Arya";
-					if (mail($row["username"], $subject, $message, $from)) {
-						//echo "mail send successfully";
-					} else {
-						//echo "Error: Cannot send mail";
-					}
-					exit(); //if login is successful it will send the mail and exit the program 
-				} 				
+$result = $obj->query($select);
+if ($result->rowCount() >= 0) {
+	while ($row = $result->fetch()) {
+		//echo "username ".$row["username"]."<br>";
+		if ($row["username"] == $user && $row["passwords"] == $password) {
+			echo "login successful<br>";
+			header("Location: components/project/project.php");
+
+			// send mail code
+			$subject = "Registration successful";
+			$message = "Welcome to our page. <br> thank u";
+			$from = "From: Naitik Arya";
+			if (mail($row["username"], $subject, $message, $from)) {
+				//echo "mail send successfully";
+			} else {
+				//echo "Error: Cannot send mail";
 			}
-			echo "Inavalid user name or password";
+			exit(); //if login is successful it will send the mail and exit the program 
 		}
-		else
-		{
-			echo "Invalid user name or password";// if my database is empty
-		}
- 
-
-=======
-<?php
-	include 'connection.php';
-	$obj = new database_connection();
-    //$obj->connection();
-
-	$user = $_POST['username'];
-	$password = md5($_POST['password']);
-	//echo "<script> alert('$user') </script>";
-		//select statement is used to get the value of username and password from database
-		$select= "SELECT username, passwords FROM user";
-		
-		$result= $obj->connection()->query($select);
-		if ($result -> rowCount() >= 0) 
-		{
-			while ($row = $result -> fetch()) 
-			{
-				//echo "username ".$row["username"]."<br>";
-				if ($row["username"]== $user && $row["passwords"]== $password) 
-				{
-					echo "login successful<br>";
-                    header("Location: components/project/project.php");
-					
-				    // send mail code
-					$subject="Registration successful";
-					$message="Welcome to our page. <br> thank u";
-					$from="From: Naitik Arya";
-					if (mail($row["username"], $subject, $message, $from)) {
-						//echo "mail send successfully";
-					} else {
-						//echo "Error: Cannot send mail";
-					}
-					exit(); //if login is successful it will send the mail and exit the program 
-				} 				
-			}
-			echo "Inavalid user name or password";
-		}
-		else
-		{
-			echo "Invalid user name or password";// if my database is empty
-		}
- 
-
->>>>>>> b1aba4acf057b25d4da68ec4258d9f84bd09353d:signin.php
-?>
+	}
+	echo "Inavalid user name or password";
+} else {
+	echo "Invalid user name or password"; // if my database is empty
+}
