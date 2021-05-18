df8c9e2633f12c04fdd862af25e2a018115b8070
Deon Graicas <deongracias1@gmail.com>
2021-05-18 11:37:01 +0530

    Restyled

--- components/issues/addissue.php
+++ components/issues/addissue.php
@@ -1,72 +1,50 @@
-<<<<<<< HEAD:components/issues/addissue.php
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../styles/globals.css">
+    <link rel="stylesheet" href="../../styles/main.css">
+    <link rel="stylesheet" href="../../styles/navbar.css">
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
 </head>
+
 <body>
-    <?php include 'menubar.php';?>
-    
-    <div class="container-1">
-        <h1>New Issue</h1>
-        <form action="">
-            <input type="text" placeholder="Issue Name" name="" class="inputbox">
-            <br>
-            <input type="text" onfocus="(this.type='date')" placeholder="Start Date" name="" class="inputbox">
-            <br>
-            <input type="text" onfocus="(this.type='date')" placeholder="End Date" name="" class="inputbox" >
-            <br>
-            <select name="" class="inputbox">
-                <option value="">Select Priority</option>
-                <option value="Low">Low</option>
-                <option value="medium">Medium</option>
-                <option value="Top">Top</option>
-            </select>
-            <br>
-            <textarea rows="4" class="inputbox" placeholder="Issue Summary"></textarea>
-            <br>
-            <button class="material-icons submit-button"> send</button>
+    <?php include '../menubar.php'; ?>
+
+    <div class="container">
+        <form class="form-container" action="">
+            <div class="header">New Issue</div>
+
+            <!-- Issue Name -->
+            <input type="text" placeholder="Issue Name" name="" class="textfield">
+
+            <!-- Issue Start Date -->
+            <input type="text" onfocus="(this.type='date')" placeholder="Start Date" name="" class="textfield">
+
+            <!-- Issue End Date -->
+            <input type="text" onfocus="(this.type='date')" placeholder="End Date" name="" class="textfield">
+
+            <!-- Issue Priority -->
+            <div class="combo-box">
+                <select name="">
+                    <option value="">Select Priority</option>
+                    <option value="low">Low</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
+                </select>
+            </div>
+
+            <!-- Issue Summary -->
+            <textarea rows="4" class="textarea" placeholder="Issue Summary"></textarea>
+
+            <!-- Submit Button -->
+            <button class="submit-btn">Submit</button>
         </form>
     </div>
-    
+
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
-</head>
-<body>
-    <?php include '../../menubar.php';?>
-    
-    <div class="container-1">
-        <h1>New Issue</h1>
-        <form action="">
-            <input type="text" placeholder="Issue Name" name="" class="inputbox">
-            <br>
-            <input type="text" onfocus="(this.type='date')" placeholder="Start Date" name="" class="inputbox">
-            <br>
-            <input type="text" onfocus="(this.type='date')" placeholder="End Date" name="" class="inputbox" >
-            <br>
-            <select name="" class="inputbox">
-                <option value="">Select Priority</option>
-                <option value="Low">Low</option>
-                <option value="medium">Medium</option>
-                <option value="Top">Top</option>
-            </select>
-            <br>
-            <textarea rows="4" class="inputbox" placeholder="Issue Summary"></textarea>
-            <br>
-            <button class="material-icons submit-button"> send</button>
-        </form>
-    </div>
-    
-</body>
->>>>>>> b1aba4acf057b25d4da68ec4258d9f84bd09353d:components/issue/addissue.php
+
 </html>
