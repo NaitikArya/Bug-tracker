291ccd1f790b0f9f58828fd299ee3bdd27df5d71
Deon Graicas <deongracias1@gmail.com>
2021-05-28 16:05:18 +0530

    added basic code

--- components/issues/resolve.php
+++ components/issues/resolve.php
@@ -1,48 +1,30 @@
-<<<<<<< HEAD:components/issues/resolve.php
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="../../styles/globals.css">
+    <link rel="stylesheet" href="../../styles/main.css">
+    <link rel="stylesheet" href="../../styles/navbar.css">
 </head>
-<body>
-    <?php include 'menubar.php';?>
-    <div class="container-1">
-        <form action="">
-            
-            <textarea rows="10" class="resolve-reply" placeholder="Resolve Reply"></textarea>
-            <br>
-            <button class="material-icons submit-button"> send</button>
 
-
-         </form>
-    </div>
-</body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
-</head>
 <body>
 
-    <?php include '../../menubar.php';?>
-    
-    <div class="container-1">
-        <form action="">
-            
-            <textarea rows="10" class="resolve-reply" placeholder="Resolve Reply"></textarea>
-            <br>
-            <button class="material-icons submit-button"> send</button>
+    <?php include '../menubar.php'; ?>
+    <div class="container">
+        <form action="" class="form-container">
+            <div class="header">
+                Resolve
+            </div>
 
+            <textarea class="textarea" placeholder="Resolve Reply"></textarea>
+            <button class="submit-btn">Send</button>
 
-         </form>
+        </form>
     </div>
 </body>
->>>>>>> b1aba4acf057b25d4da68ec4258d9f84bd09353d:components/issue/resolve.php
+
 </html>
