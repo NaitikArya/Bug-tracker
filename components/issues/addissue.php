c88b616128e13e97e570f0c9d799f0a55512f4ac
Deon Graicas <deongracias1@gmail.com>
2021-05-28 16:05:07 +0530

    addissue completed

---
+++ components/issues/addissue.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once("../../helper/connection.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $conn = new Connection();
+
+    $issueName = $_POST["issueName"];
+    $startDate = date($_POST["startDate"]);
+    $endDate = date($_POST["endDate"]);
+    $priority = $_POST["priority"] == "low" ? 0 : ($_POST["priority"] ? 1 : 2);
+    $issueSummary = $_POST["summary"];
+
+    $conn->insertData("INSERT INTO issues(issue_name, startDate, endDate, priority, issue_summary) VALUES ('$issueName',$startDate,$endDate,$priority,'$issueSummary')");
+}
+
+
+$url = "../../pages/issue.php";
+
+ob_start();
+header('Location: ' . $url);
+ob_end_flush();
+die();
